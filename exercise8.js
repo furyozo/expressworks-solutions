4990ea8235c4971e6ac849ccb2e6e89e2f5d0685
furyozo <bodyboro@seznam.cz>
2018-02-04 13:15:08 +0100

    adding expressworks solutions

---
+++ exercise8.js
@@ -0,0 +1,12 @@
+var fs = require('fs')
+var express = require('express')
+var app = express()
+
+app.get('/books', function(req, res) {
+  fs.readFile(process.argv[3], function(err, data) {
+    if (err) throw(err)
+    res.json(JSON.parse(data.toString()))
+  })
+})
+
+app.listen(process.argv[2])
